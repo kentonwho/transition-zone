50537c9557c6899ec8ac7021ba634e5c9f7b0824
kentonwho <wukenton@gmail.com>
2024-05-29 10:25:15 -0500

    prep for designsafe

---
+++ fixname.sh
@@ -0,0 +1,6 @@
+ncrename -v neta,neta_rm -v nvel,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
+ncrename -v neta,neta_rm -v nvel,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
+ncrename -v neta,neta_rm -v nvel,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
+ncks -x -v neta_rm,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
+ncks -x -v neta_rm,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
+ncks -x -v neta_rm,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
