5992c57a416e088a6ba95f2cc86e31c745e313b6
Kenton Wu <wukenton@gmail.com>
2024-06-14 21:09:48 +0000

    stable working designsafe build

--- fixname.sh
+++ fixname.sh
@@ -1,6 +1,9 @@
-ncrename -v neta,neta_rm -v nvel,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
-ncrename -v neta,neta_rm -v nvel,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
-ncrename -v neta,neta_rm -v nvel,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
-ncks -x -v neta_rm,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
-ncks -x -v neta_rm,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
-ncks -x -v neta_rm,nvel_rm ike_compound.63.nc ike_compound.63.nc --overwrite
+#This script is a hotfix for the ADCIRC bug that malforms the names of the 'neta' and 'nvel' variables
+name="harvey"
+
+ncrename -v neta,neta_rm -v nvel,nvel_rm ${name}_compound.63.nc ${name}_compound.63.nc --overwrite
+ncrename -v neta,neta_rm -v nvel,nvel_rm ${name}_surge.63.nc ${name}_surge.63.nc --overwrite
+ncrename -v neta,neta_rm -v nvel,nvel_rm ${name}_rivers.63.nc ${name}_rivers.63.nc --overwrite
+ncks -x -v neta_rm,nvel_rm ${name}_compound.63.nc ${name}_compound.63.nc --overwrite
+ncks -x -v neta_rm,nvel_rm ${name}_surge.63.nc ${name}_surge.63.nc --overwrite
+ncks -x -v neta_rm,nvel_rm ${name}_rivers.63.nc ${name}_rivers.63.nc --overwrite
